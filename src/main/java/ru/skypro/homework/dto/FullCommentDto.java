7fc166a5a8738db09eb60d761e547c2549fe2c0d
roma <rommur5@mail.ru>
2024-03-28 20:47:43 +0300

    create CommentController and GetMapping

---
+++ src/main/java/ru/skypro/homework/dto/FullCommentDto.java
@@ -0,0 +1,18 @@
+package ru.skypro.homework.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import ru.skypro.homework.entity.User;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class FullCommentDto {
+    private Integer author;
+    private String authorImage;
+    private String authorFirstName;
+    private Long createAt;
+    private Integer pk;
+    private String text;
+}
